17a0b9f60724e783671116d0aed33b33e810cae3
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2018-01-09 11:01:50 +0100

    Fix

--- Examples/NewNavUser.ps1
+++ Examples/NewNavUser.ps1
@@ -3,9 +3,14 @@
 #$ADuser = "si-data\jab123243"
 $NavServiceInstance = "IncadeaAddOns"
 $NavServiceInstance = "DynamicsNAV100"
+$NavServiceInstance = "NAV100_TP"
+$ADuser = "si-dev\devjal"
 
 Set-ExecutionPolicy RemoteSigned -Force
-##Import-Module "C:\Program Files\Microsoft Dynamics NAV\71\Service\NavAdminTool.ps1"
+Import-Module "C:\Program Files\Microsoft Dynamics NAV\100\Service CU03\NavAdminTool.ps1"
+#Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\100\RTC CU03\Microsoft.Dynamics.Nav.Model.Tools.psd1" 
+#Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\100\RTC CU03\Microsoft.Dynamics.Nav.Apps.Tools.psd1" 
+
 
 Get-NAVServerInstance $NavServiceInstance | New-NAVServerUser -WindowsAccount $ADuser 
 Get-NAVServerInstance $NavServiceInstance | New-NAVServerUserPermissionSet –WindowsAccount $ADuser -PermissionSetId SUPER -Verbose
