6bc0c52ccbcec557766442bdcd56a352215165e0
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2018-01-15 14:30:29 +0100

    Import users

--- Examples/ImportUser2NAV.ps1
+++ Examples/ImportUser2NAV.ps1
@@ -1,64 +1,42 @@
-﻿#Import NAV admin module for the version of Dynamics NAV you are using 
-Import-Module 'C:\Program Files\Microsoft Dynamics NAV\80\Service\NavAdminTool.ps1' 
+﻿Set-ExecutionPolicy RemoteSigned -Force
+#Import NAV admin module for the version of Dynamics NAV you are using 
+Import-Module "C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1"
+$NavServiceInstance = "NAVOAS"
 
-Set-Location "C:\si-data\Data\"
-
-$NavServerName ="DynamicsNAV80"
-
+Set-Location "C:\Incadea\Data\"
 $ImportFilePath = "UsersAndRoles.csv"
-#$ImportFilePath = "UsersAndRolesDOSFormat.csv"
 
-$list = Import-csv -Path $ImportFilePath -Delimiter ';' -Header sid,username,rolesid,company
-$DomainName = "SPILKAD"
+$list = Import-csv -Path $ImportFilePath -Encoding UTF8 -Delimiter ';' -Header username,fullname,roleset,company
+$DomainName = "overasen"
 
+#$list = Import-csv -Path $ImportFilePath -Encoding Unicode -Delimiter ';' -Header username,fullname,roleset
 foreach ($user in $list) 
 {
-    #$WindowsUser = Get-WMIObject Win32_UserAccount | Where-Object {$_.FullName -eq $user.username -and $_.Domain -eq $DomainName}
-    $WindowsUser = Get-WMIObject Win32_UserAccount | Where-Object {$_.SID -eq $user.sid}
-    if([String]::IsNullOrEmpty($WindowsUser.SID))
-    {
-        $navuser = Get-NAVServerUser -ServerInstance $NavServerName |  where-Object UserName -eq $user.username
-    }else
-    {
-        $navuser = Get-NAVServerUser -ServerInstance $NavServerName |  where-Object WindowsSecurityID -eq $WindowsUser.SID
-    }
-	
-    if([String]::IsNullOrEmpty($navuser.UserName))
-    { 
-        "New user: " + $user.username
-        $navuser = New-NAVServerUser -ServerInstance $NavServerName -WindowsAccount $WindowsUser.Caption -FullName $WindowsUser.FullName -ErrorAction Continue
-        #$navuser = New-NAVServerUser -ServerInstance $NavServerName -WindowsAccount $user.username -ErrorAction Continue
-    }
-    else
-    {
-        "User '" + $user.username + "' is already registered in NAV"
-    }
+    $navuser= Get-NAVServerUser -ServerInstance $NavServiceInstance
+    #Write-Host $navuser.UserName  $user.username
+    $NAVUserName = $DomainName + '\' + $user.username
 
-    $navroleExists = Get-NAVServerPermissionSet -ServerInstance $NavServerName | where-Object PermissionSetID -eq $user.rolesid
-    if (!([String]::IsNullOrEmpty($navroleExists)))
-    {
-		$navrole = Get-NAVServerUserPermissionSet -ServerInstance $NavServerName -UserName $user.username
-        #$navrole = Get-NAVServerUserPermissionSet -ServerInstance $NavServerName -WindowsAccount $WindowsUser.Caption
-        #New-NAVServerUserPermissionSet -PermissionSetId SUPER -ServerInstance DynamicsNAV80 -WindowsAccount OSOHOTWATER\kf
-        #$navrole
-        #$user.rolesid
-        #$navrole.PermissionSetID 
-        if((!($navrole.PermissionSetID -contains $user.rolesid)) -or ([String]::IsNullOrEmpty($navrole)))
-        {
-            #New-NAVServerUserPermissionSet -ServerInstance $NavServerName -WindowsAccount $user.username -PermissionSetId $user.rolesid -CompanyName $user.company -ErrorAction Continue
-            #New-NAVServerUserPermissionSet -ServerInstance $NavServerName -WindowsAccount $WindowsUser.Caption -PermissionSetId $user.rolesid -ErrorAction Continue
-			New-NAVServerUserPermissionSet -ServerInstance $NavServerName -UserName $user.username -CompanyName $user.company -PermissionSetId $user.rolesid -ErrorAction Continue
-            "New Role for '" + $user.username + "': " + $user.rolesid + " ; " + $user.company
-        }
-        else
-        {
-            "User '" + $user.username + "' already has the role '" + $user.rolesid + "'."
-        }
+    if(!($navuser.UserName -contains $user.username))
+    { 
+        New-NAVServerUser -ServerInstance $NavServiceInstance -WindowsAccount $NAVUserName -FullName $user.fullname -ErrorAction Continue
     }
     else
     {
-        "Role ID '" + $user.rolesid + "' does not exists."
+        Set-NAVServerUser -ServerInstance $NavServiceInstance -WindowsAccount $NAVUserName -FullName $user.fullname -ErrorAction Continue
     }
+    #In the csv file used in this example, the list of roles is divided by a comma 
+    $roleset=$user.roleset.Split(',')
+    foreach ($role in $roleset)
+    {
+        $navrole=Get-NAVServerUserPermissionSet -ServerInstance $NavServiceInstance -WindowsAccount $user.username
+        if(!($navrole.PermissionSetID -contains $role))
+        {
+            New-NAVServerUserPermissionSet -ServerInstance $NavServiceInstance -WindowsAccount $NAVUserName -CompanyName $user.company -PermissionSetId $role -ErrorAction Continue
+            #Remove-NAVServerPermissionSet ServerInstance $NavServiceInstance -WindowsAccount $NAVUserName
+        }
+        
+    }
+    #Remove-NAVServerUser -ServerInstance $NavServiceInstance -WindowsAccount $NAVUserName
 }
 
  
