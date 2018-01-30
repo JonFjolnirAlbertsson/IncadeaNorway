0987e3e9365e533a5a0867c4ba829800b0e01cfe
Jón Fjölnir Albertsson <fjolnir.albertsson@incadea.com>
2018-01-30 10:21:18 +0100

    Adde new function New-SQLUser-INC

---
+++ NAVModules/New-SQLUser-INC.ps1
@@ -0,0 +1,38 @@
+﻿function New-SQLUser-INC
+{
+    <#
+    .Synopsis
+       Creating  db user and adding db role 
+    .DESCRIPTION
+       Creating  db user and adding db role 
+    .NOTES
+       No Return value
+    .PREREQUISITES
+   
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [String] $DatabaseServer = '.',
+        [Parameter(Mandatory=$false)]
+        [String] $DatabaseInstance = '',
+        [Parameter(Mandatory=$true)]
+        [String] $DatabaseName,
+        [Parameter(Mandatory=$true)]
+        [String] $DatabaseUser = '',
+        [Parameter(Mandatory=$false)]
+        [String] $DatabaseRole = 'db_owner',
+		[Parameter(Mandatory=$false)]
+        [String] $TimeOut = 0,
+        [Parameter(Mandatory=$false)]
+        [switch]$TrustedConnection
+    )
+    $SQLString = "CREATE USER [$DatabaseUser] FOR LOGIN [$DatabaseUser]"
+    write-Host -ForegroundColor Green "Starting command $SQLString]"
+    Invoke-sql-INC -DatabaseServer $DatabaseServer -DatabaseInstance $DatabaseInstance -TrustedConnection $TrustedConnection -TimeOut $TimeOut -sqlCommand $SQLString
+    $SQLString = "ALTER ROLE [db_owner] ADD MEMBER [$DatabaseUser]"
+    write-Host -ForegroundColor Green "Starting command $SQLString]"
+    Invoke-sql-INC -DatabaseServer $DatabaseServer -DatabaseInstance $DatabaseInstance -TrustedConnection $TrustedConnection -TimeOut $TimeOut -sqlCommand $SQLString
+}
+
